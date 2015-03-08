33c27af783e2fbd5af02ce0171a605a8dc8c80e4
özge <ozzgee.kaynak@hotmail.com>
2015-03-08 15:45:23 +0200

    Data Structure
    Arrays, Matrices, Methods, Random Numbers kullanıma yönelik bir
    projedir.Birinci programda bilgisayara random ile doğum günü tarihleri
    ürettik ve bu tarihlerin çakışma yüzdeliklerini ve tarihlerini
    gösterdik.
    İkinci projede ise öğrencilerin 9 ülkeye (ENG, GER, FRE, ITA, ESP, USA,
    JAP, CHN, RUS) yerleştirmeyi yapan yazılım geliştirmemiz isteniyor.

---
+++ PROJE1-PROGRAM1/Program.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace ilk1
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static ArrayList array = new ArrayList();
+        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Görmek istediğiniz işlemin numarasını giriniz:");
+            Console.WriteLine("1-(01.01.1995-31.12.1997) yılları arasında bir tarih");
+            Console.WriteLine("2-(01.01.1995-31.12.1997) yılları arasında istediginiz kadar tarih");
+            Console.WriteLine("3-50,100,500,1000 kişilik doğum yılıları dahil çakışma listesi ve çakışan tarihlerin gösterimi");
+            Console.WriteLine("4-50,100,500,1000 kişilik çakışma listesi ve çakışan tarihlerin gösterimi");
+            Console.Write("Şıkkınız :");
+            int sayi = Convert.ToInt16(Console.ReadLine());
+            int gun, ay, yıl;
+
+            int[] gunler = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            switch (sayi)
+            {
+                case 1:
+                    ay = rnd.Next(0, 12);
+                    gun = rnd.Next(0, gunler[ay]);
+                    yıl = rnd.Next(1995, 1998);
+                    Console.WriteLine(gun + "." + ay + "." + yıl);
+                    Console.ReadLine();
+                   
+                    break;
+                case 2:
+                    Console.WriteLine("görmek istediginiz tarih sayısını giriniz:");
+                    int n = Convert.ToInt16(Console.ReadLine());
+                    for (int i = 0; i < n; i++)
+                    {
+                        ay = rnd.Next(0, 12);
+                        gun = rnd.Next(0, gunler[ay]);
+                        yıl = rnd.Next(1995, 1998);
+                        Console.WriteLine(gun + "." + ay + "." + yıl);
+
+                    }
+                    Console.ReadLine();
+                    break;
+                case 3:
+                    int[,] mtr2 = new int[4, 11];
+                    int[, ,] mtr = new int[12, 31, 3];
+                    int[] dizi1 = { 50, 100, 500, 1000 };
+                    int toplam = 0;
+                    int ort = 0;
+                    
+
+                    for (int index = 0; index < 4; index++)
+                    {
+                        Console.WriteLine("n=" + dizi1[index] + "  için degerler:");
+                        
+                        for (int i = 0; i <10; i++)
+                        {
+                            ucboyutmatrisSfr(mtr, 12, 31, 3);
+                            for (int j = 0; j < dizi1[index]; j++)
+                            {
+
+                                ay = rnd.Next(0, 12);
+                                gun = rnd.Next(0, gunler[ay]);
+                                yıl = rnd.Next(1995, 1998);
+                                mtr[ay, gun, yıl - 1995]++;                               
+                            }
+
+                            for (int a = 0; a < 11; a++)
+                            {
+                                for (int b = 0; b < 31; b++)
+                                {
+                                    for (int c = 0; c < 3; c++)
+                                    {
+                                        if (mtr[a, b, c] > 1)
+                                        {
+                                            toplam += mtr[a, b, c] - 1;
+
+                                        }
+                                    }
+                                }
+                            }
+                            mtr2[index, i] = toplam;
+                            toplam = 0;
+                            ucBoyutlumatris_atama(mtr);                           
+                        }
+                        ucBoyutluYazdir();
+                        Console.ReadLine();
+                    }
+                 
+                    for (int c = 0; c < 4; c++)
+                    {
+                        Console.WriteLine("\n");
+                        Console.Write(dizi1[c] + "kişilik-->");
+                        for (int i = 0; i < 10; i++)
+                        {
+                            Console.Write("{0,2}", mtr2[c, i] + " ");
+                            ort += mtr2[c, i];
+                        }
+                        Console.WriteLine((float)ort / 10);
+                        ort = 0;
+                    }
+
+                    Console.ReadLine();
+
+                    break;
+
+                case 4:
+                    int[,] matris2 = new int[4, 11];
+                    int[,] matris = new int[12, 31];
+                    int[] dizi = { 50, 100, 500, 1000 };
+                    int tpl = 0;
+                    int ortalama = 0;
+
+
+                    for (int index = 0; index < 4; index++)
+                    {
+                        Console.WriteLine("n=" + dizi[index] + "  için degerler:");
+                        for (int i = 0; i <10; i++)
+                        {
+                            matrisSıfırla(matris, 12, 31);
+                            for (int j = 0; j < dizi[index]; j++)
+                            {
+
+                                ay = rnd.Next(0, 12);
+                                gun = rnd.Next(0, gunler[ay]);
+                                matris[ay, gun]++;
+                            }
+
+                            for (int a = 0; a < 11; a++)
+                            {
+                                for (int b = 0; b < 31; b++)
+                                {
+
+                                    if (matris[a, b] > 1)
+                                    {
+                                        tpl += matris[a, b] - 1;
+                                    }
+                                }
+                            }
+                            matris2[index, i] = tpl;
+                            tpl = 0;
+                            matris_yazdır(matris);
+                        }
+                        Console.ReadLine();
+
+                    }
+                    for (int c = 0; c < 4; c++)
+                    {
+                        Console.WriteLine("\n");
+                        Console.Write(dizi[c] + "kişilik-->");
+                        for (int i = 0; i < 10; i++)
+                        {
+                            Console.Write(matris2[c, i] + " ");
+                            ortalama += matris2[c, i];
+                        }
+                        Console.WriteLine((float)ortalama / 10);
+                        ortalama = 0;
+                    }
+
+                    Console.ReadLine();
+                    break;
+            }
+        }
+        public static void matris_yazdır(int[,] matris)
+        {
+            string[] aylar = {"Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık" };
+            for (int i = 0; i < 12; i++)
+            {
+                Console.Write("{0,7}",aylar[i]);
+                for (int j = 0; j < 31; j++)
+                {
+                    Console.Write("{0,2}", matris[i, j]);
+                }
+
+                Console.WriteLine(); 
+            }
+            Console.WriteLine("\n" + "\n");
+        }
+        public static void ucBoyutlumatris_atama(int[, ,] matris)
+        {
+            int[,] mtr = new int[12, 31];
+            for (int k = 0; k < 3; ++k)
+            {
+                matrisSıfırla(mtr,12,31);
+                for (int i = 0; i < 12; i++)
+                {
+                    for (int j = 0; j < 31; j++)
+                    {
+                        mtr[i, j] = matris[i, j, k];
+                    } 
+                }
+                array.Add(mtr);
+            }
+        }
+        public static void ucBoyutluYazdir()
+        {
+            string[] aylar = { "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık" };
+            int i=0;
+           
+            Console.WriteLine("1995 Yılında Dogmus Kisiler \n");
+              while(i<array.Count)
+             {
+                 if (array[i] != null)
+                 {
+                     for (int k = 0; k < 12; k++)
+                     {
+                         Console.Write("{0,7}", aylar[k]);
+                         for (int j = 0; j < 31; j++)
+                         {
+
+                             Console.Write("{0,2}", ((int[,])array[i])[k, j]);
+
+                         }
+
+                         Console.WriteLine();
+                     }
+                     Console.WriteLine("\n" + "\n");
+                 }
+                array[i] = null;
+                i += 3;
+                
+            }
+              Console.ReadLine();
+
+              i = 1;
+              Console.WriteLine("1996 Yılında Dogmus Kisiler \n");
+             
+            
+            while (i < array.Count)
+              {
+                  if (array[i] != null)
+                  {
+                      for (int k = 0; k < 12; k++)
+                      {
+                          Console.Write("{0,7}", aylar[k]);
+                          for (int j = 0; j < 31; j++)
+                          {
+
+                              Console.Write("{0,2}", ((int[,])array[i])[k, j]);
+
+                          }
+
+                          Console.WriteLine();
+                      }
+                      Console.WriteLine("\n" + "\n");
+                  }
+                  array[i] = null;
+                  i += 3;
+                 
+              }
+
+              Console.ReadLine();
+              i = 2;
+              Console.WriteLine("1997 Yılında Dogmus Kisiler \n");
+              
+            
+            while (i < array.Count)
+              {
+                  if (array[i] != null)
+                  {
+                      for (int k = 0; k < 12; k++)
+                      {
+
+                          Console.Write("{0,7}", aylar[k]);
+                          for (int j = 0; j < 31; j++)
+                          {
+
+                              Console.Write("{0,2}", ((int[,])array[i])[k, j]);
+
+                          }
+                          Console.WriteLine();
+                      }
+
+                   Console.WriteLine("\n" + "\n");   
+                  }
+                  array[i] = null;
+                  i += 3;
+                  
+              }
+        }
+        
+        public static void matrisSıfırla(int[,] matris2, int satir, int sutun)
+        {
+            for (int i = 0; i < satir; i++)
+            {
+                for (int j = 0; j < sutun; j++)
+                    matris2[i, j] = 0;
+            }
+        }
+        public static void ucboyutmatrisSfr(int[, ,] matris2, int ayi, int gunu, int yılı)
+        {
+            for (int i = 0; i < ayi; i++)
+            {
+                for (int j = 0; j < gunu; j++)
+                {
+                    for (int k = 0; k < yılı; k++)
+                        matris2[i, j, k] = 0;
+                }
+            }
+        }
+    }
+}
---
+++ PROJE_1_PROGRAM_2/Program.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace _2.proje
+{  
+    class Program
+    {
+        static Random rnd = new Random();
+        static Ülke[] ülke = new Ülke[9];
+        static int ülke_sıra=0;
+        static void Main(string[] args)
+        {
+             int ögr_say = 0;
+             Ülke[] ülkeler=kontenjanAl();
+           
+             Ögrenci[] bilgi =  ögrenciBilgileri();
+             foreach (object a in bilgi)
+             {
+                 ögr_say++;
+             }
+
+             ögrenci_yerlesdir(ülkeler, bilgi, ögr_say);
+
+             Console.ReadLine();
+        }
+
+
+        public static void ögrenci_yerlesdir(Ülke[] ü, Ögrenci[] bilgi, int sayi)
+        {
+            int j, toplam_kontenjan = 0, gecenÖgr = 0, say = 0;
+            double[] kznÖgr = new double[9];
+            double x;
+
+
+            for (j = 0; j < 9; j++)
+            {
+                toplam_kontenjan += ü[j].kontenjan;
+            }
+
+            int[] not_dizisi = new int[sayi];
+            for (int o = 0; o < sayi; o++)
+            {
+                if (bilgi[o].not > 60)
+                {
+                    gecenÖgr++;
+                    not_dizisi[say] = bilgi[o].not;
+                    say++;
+                }
+            }
+
+            int[] notSıralaması = new int[gecenÖgr];
+
+            for (int m = 0; m < gecenÖgr; m++)
+            {
+                for (int n = m + 1; n < gecenÖgr; n++)
+                {
+                    if (not_dizisi[n] > not_dizisi[m])
+                    {
+                        notSıralaması[m] = not_dizisi[n];
+                        not_dizisi[n] = not_dizisi[m];
+                        not_dizisi[m] = notSıralaması[m];
+                    } 
+                 
+                } bilgi[m].not = not_dizisi[m];
+            }
+         
+            string[] kontenjan_olmayan= new string[9];
+            double basarılı = 0;
+            for (int c = 0; c < 9; c++)
+            {
+                x = ü[c].kontenjan;
+                kznÖgr[c] = Math.Round((x * gecenÖgr / toplam_kontenjan));
+                if (ü[c].kontenjan == 0)
+                {
+                    kontenjan_olmayan[c] = ü[c].adı;
+                }
+                basarılı += kznÖgr[c];
+   
+            }
+            int[] sıralı_ülke_kontenjan = new int[9];
+            double[] sıralı_kznöğr = new double[9];
+            string[] ülke_ad_sırala = new string[9];
+            for (int m = 0; m < 9; m++)
+            {
+                for (int n = m + 1; n < 9; n++)
+                {
+                    if (kznÖgr[n] > kznÖgr[m])
+                    {
+                        sıralı_kznöğr[m] = kznÖgr[n];
+                        kznÖgr[n] = kznÖgr[m];
+                        kznÖgr[m] = sıralı_kznöğr[m];
+                        ülke_ad_sırala[m] = ü[n].adı;
+                        ü[n].adı = ü[m].adı;
+                        ü[m].adı = ülke_ad_sırala[m];
+                        sıralı_ülke_kontenjan[m] = ü[n].kontenjan;
+                        ü[n].kontenjan = ü[m].kontenjan;
+                        ü[m].kontenjan = sıralı_ülke_kontenjan[m];
+                    }
+                }
+            }
+                for (int k = 0; k < 9; k++)
+                {
+                    if (kznÖgr[k] > ü[k].kontenjan)
+                    {
+                        kznÖgr[k] = ü[k].kontenjan;
+                    }
+                   
+                    while (basarılı > gecenÖgr)
+                    {
+                        if (kznÖgr[k] > kznÖgr[k + 1])
+                            kznÖgr[k]--;
+                        else
+                            kznÖgr[k + 1]--;
+                        basarılı--;
+                    }
+
+                     if (kznÖgr[k] == 0 && kontenjan_olmayan[k] != null)
+                         Console.WriteLine("***" + ü[k].adı + " ülkesi bu sene kontenjan açmamıştır!!!");
+                     else
+                     {
+                         if (kznÖgr[k] == 0)
+                             Console.WriteLine("***" + ü[k].adı + "  ülkesine bu sene  ögrenci gitmemektedir...");
+                         else
+                             Console.WriteLine("***" + ü[k].adı + "  ülkesine " + kznÖgr[k] + "  ögrenci gitmektedir...");
+                         öğr_yazdır(kznÖgr[k], bilgi, gecenÖgr);   
+                     }         
+                        Console.WriteLine("\n");  
+                }
+
+                Console.WriteLine("********************************************************");
+                if (gecenÖgr - basarılı == 0)
+                    Console.WriteLine("Tüm öğrenciler yerleşmiştir!!!");
+
+                else
+                {
+                    Console.WriteLine("***Yerleştirilemeyen öğrenciler***");
+                    öğr_yazdır(gecenÖgr-basarılı, bilgi, gecenÖgr);
+                }
+            }
+        
+        public static void öğr_yazdır(double kontenjan, Ögrenci[] bilgi,int gecen_ögr)
+        {
+            ülke[ülke_sıra] = new Ülke();
+                int say = 0, i = 0;
+
+                if (kontenjan != 0)
+                {
+                    Console.WriteLine("  AD           SOYAD         NOT");
+                    Console.WriteLine("*******    *************   *******");
+                    do
+                    {
+                        if (bilgi[i].isim != null && bilgi[i].soyad != null && bilgi[i].not != 0  )
+                        {
+                            
+                            ülke[ülke_sıra].talebeler[say] = bilgi[i];
+                            Console.WriteLine("{0,-15}{1,-10}{2,7}", ülke[ülke_sıra].talebeler[say].isim, ülke[ülke_sıra].talebeler[say].soyad, ülke[ülke_sıra].talebeler[say].not);
+                            say++;
+                        }
+                        bilgi[i].isim = null;
+                        bilgi[i].soyad = null;
+                        bilgi[i].not = 0;
+
+                        i++;
+                    } while (say != kontenjan && i <= gecen_ögr);
+                }
+            if(ülke_sıra<8)
+            ülke_sıra++;
+        }
+        public static  Ögrenci[]  ögrenciBilgileri()
+        {
+            int ogrSay, say1, say2;
+            string[] ad_dizisi = { "Elanur", "Özge", "Gül", "Nebahat", "Rabiye", "Songül", "Dilek", "Aybüke", "Betül", "Taner", "Ersan", "İhsan", "Tolga", "Oğuz", "Burak", "Hasan", "Hüseyin", "Mahmut", "Umut", "Selim", "Erdi" };
+            string[] soyad_dizisi = { "Kaynak", "Bilgin", "Aydoğan", "Çolak", "Oral", "Bayram", "Kara", "Hazer", "Sönmez", "Öztürk", "Kurt", "Onay", "Gümüş", "Aktaş", "Şimşek", "Uysal", "Yaprak", "Yüksel", "Çolak", "Gürsoy" };
+           
+            do
+            {
+                Console.WriteLine("[1-150] arasında başvurabilecek öğrenci sayısını giriniz:");
+                ogrSay = Convert.ToInt16(Console.ReadLine());
+            } while (ogrSay >150 || ogrSay <1);
+          
+            Ögrenci[] kişiler = new Ögrenci[ogrSay];
+            for(int i=0;i<ogrSay;i++)
+            { 
+                say1 = rnd.Next(0, 20);
+                say2 = rnd.Next(0, 20);
+                kişiler[i] = new Ögrenci();
+                kişiler[i].isim=ad_dizisi[say1];
+                kişiler[i].soyad=soyad_dizisi[say2];
+                kişiler[i].not = rnd.Next(40, 100);   
+            }
+            return kişiler;
+        }
+        public static Ülke[] kontenjanAl()
+        {  
+            Ülke[] ülkeler=new Ülke[9];
+
+            string[] ad = { "ENG", " GER", "FRE", "ITA", "ESP", "USA", "JAP", "CHN", "RUS" };
+            int[] sayi = new int[9];
+            string cevap;
+            int i, j;
+        Console.WriteLine("Kontenjan degerini atamak ister misiniz?{e/E/H/h}");
+        cevap = Console.ReadLine();
+        if (cevap != "e" && cevap != "E" && cevap != "h" && cevap != "H")
+        {
+            do
+            {
+                Console.WriteLine("Lütfen 'e','E','H' ya da 'h' giriniz!");
+                cevap =Console.ReadLine();
+            } while (cevap != "e" && cevap != "E" && cevap != "h" && cevap != "H");
+        }
+        
+        if (cevap == "e" || cevap == "E")
+        {
+            for (i = 0; i < 9; i++)
+            {
+                ülkeler[i] = new Ülke();
+                do
+                {
+                    Console.WriteLine(ad[i] + " ülkesinin kontenjan sayısını [0,10] arasında giriniz:");
+                    sayi[i] = Convert.ToInt32(Console.ReadLine());
+                } while (sayi[i] < 0 || sayi[i] > 10);
+
+                ülkeler[i].kontenjan=sayi[i];
+                
+                ülkeler[i].adı = ad[i];
+            }
+        }
+        else if (cevap == "h" || cevap == "H")
+        {
+            for (j = 0; j < 9; j++)
+            {
+                ülkeler[j] = new Ülke();
+               sayi[j]= rnd.Next(0, 10);
+               ülkeler[j].kontenjan = sayi[j];
+               ülkeler[j].adı = ad[j];
+            }
+        }
+        return ülkeler;
+    }
+      
+ }
+    class Ülke
+    {
+        public string adı;
+        public int kontenjan;
+        public Ögrenci[] talebeler=new Ögrenci[400];
+    }
+    class Ögrenci
+    {
+        public string isim;
+        public string soyad;
+        public int not;
+    }
+}
